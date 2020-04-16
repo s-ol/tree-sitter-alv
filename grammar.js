914dbfc2d66f22d42d0a4d0714ba5b65890f9399
s-ol <s-ol@users.noreply.github.com>
2020-04-16 10:56:56 +0200

    initial commit

---
+++ grammar.js
@@ -0,0 +1,86 @@
+var str = ($, delim) => {
+  var not_delim = new RegExp('[^' + delim + ']');
+  return seq(
+    delim,
+    repeat(choice($.escape_char, not_delim)),
+    delim,
+  )
+};
+
+var exp_list = ($) => {
+  return seq(
+    optional($._sp),
+    optional(seq(
+      $._expression,
+      repeat(seq($._sp, $._expression)),
+      optional($._sp),
+    )),
+  );
+}
+
+module.exports = grammar({
+  name: 'alv',
+
+  rules: {
+    source_file: $ => exp_list($),
+
+     // wc: white-space character
+     // sp: whitespace
+    _wc: $ => /[ \t\r\n]/,
+    _sp: $ => seq(
+      repeat1(choice($.comment, $._wc)),
+    ),
+
+    // comment_nested: pair of braces
+    _comment_contents: $ => seq(
+      '(',
+      repeat(choice($._comment_contents, /[^)]/)),
+      ')',
+    ),
+    comment: $ => seq('#', $._comment_contents),
+
+    // pieces for atom definitions
+    _first: $ => /[a-zA-Z-_+*\/\.!?=%]/,
+    _digit: $ => /[0-9]/,
+    _int: $ => prec(2, repeat1($._digit)),
+    _float: $ => choice(
+      seq(repeat1($._digit), '.', repeat($._digit)),
+      seq(repeat($._digit), '.', repeat1($._digit)),
+    ),
+    escape_char: $ => choice('\"', '\'', '\\'),
+
+    // atoms
+    sym: $ => seq(
+      $._first,
+      repeat(choice($._first, $._digit)),
+    ),
+    num: $ => seq(
+      optional('-'),
+      choice($._float, $._int),
+    ),
+    str: $ => choice(
+      str($, '"'),
+      str($, '\''),
+    ),
+    _atom: $ => choice($.sym, $.num, $.str),
+
+    // expression: anything that has a value
+    // exp_list: list of expressions (potentially empty)
+    // with optional leading and trailing whitespace
+    // and required whitespace between expressions
+    _expression: $ => choice($._atom, $.cell),
+
+    tag: $ => seq(
+      '[',
+      repeat1($._digit),
+      ']',
+    ),
+
+    cell: $ => seq(
+      '(',
+      optional($.tag),
+      exp_list($),
+      ')',
+    ),
+  }
+});
