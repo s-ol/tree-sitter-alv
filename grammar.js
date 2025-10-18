2f8b25820d924720f865a163c6e260411ae41d83
s-ol <s+removethis@s-ol.nu>
2025-10-18 13:54:48 +0200

    add template strings

--- grammar.js
+++ grammar.js
@@ -13,6 +13,7 @@ var str = ($, delim) => {
 module.exports = grammar({
   name: 'alv',
 
+  extras: $ => [],
   rules: {
     source_file: $ => seq(
       optional($._sp),
@@ -32,17 +33,20 @@ module.exports = grammar({
       repeat(choice($._comment_contents, /[^)]/)),
       ')',
     ),
-    comment: $ => seq('#', $._comment_contents),
+    // comment: $ => seq('#', $._comment_contents),
+    _comment_cell: $ => seq('#', $._comment_contents),
+    _comment_line: $ => /##.*?\n/,
+    comment: $ => choice($._comment_line, $._comment_cell),
 
     // pieces for atom definitions
-    _first: $ => /[a-zA-Z-_+*\/\.=~!?%]/,
+    _first: $ => /[a-zA-Z\-_+*\/\.=~!?%]/,
     _digit: $ => /[0-9]/,
     _int: $ => prec(2, repeat1($._digit)),
     _float: $ => choice(
       seq(repeat1($._digit), '.', repeat($._digit)),
       seq(repeat($._digit), '.', repeat1($._digit)),
     ),
-    escape_char: $ => /\\["'\\]/,
+    escape_char: $ => token(prec(1, choice('\\"', "\\'", '\\\\', '\\$'))),
 
     // atoms
     sym: $ => seq(
@@ -57,13 +61,23 @@ module.exports = grammar({
       str($, '"'),
       str($, '\''),
     ),
+
+    tpl_subst: $ => seq('$', $._expression),
+    tplstr: $ => seq(
+      '$',
+      optional($.tag),
+      field($.sym, 'head'),
+      '"',
+      repeat(choice($.escape_char, $.tpl_subst, /[^"]/)),
+      '"',
+    ),
     _atom: $ => choice($.sym, $.num, $.str),
 
     // expression: anything that has a value
     // exp_list: list of expressions (potentially empty)
     // with optional leading and trailing whitespace
     // and required whitespace between expressions
-    _expression: $ => choice($._atom, $.cell),
+    _expression: $ => choice($._atom, $.cell, $.tplstr),
 
     tag: $ => seq(
       '[',
@@ -71,15 +85,13 @@ module.exports = grammar({
       ']',
     ),
 
-    // wrap head for highlghting
-    head: $ => $._expression,
     cell: $ => seq(
       '(',
       optional($.tag),
       seq(
         optional($._sp),
         optional(seq(
-          $.head,
+          field($._expression, 'head'),
           repeat(seq($._sp, $._expression)),
           optional($._sp),
         )),
